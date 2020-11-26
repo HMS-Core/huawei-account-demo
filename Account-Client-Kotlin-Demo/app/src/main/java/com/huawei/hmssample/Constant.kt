fd5d33b9da589a8b0036becf3c1fa361bf95462a
xwx973027 <xuyawen6@huawei.com>
2020-11-26 11:07:38 +0800

    AccountKit sample code update

--- Account-Client-Kotlin-Demo/app/src/main/java/com/huawei/hmssample/Constant.kt
+++ Account-Client-Kotlin-Demo/app/src/main/java/com/huawei/hmssample/Constant.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020. Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.huawei.hmssample
 
 
@@ -7,10 +22,7 @@ object Constant {
     const val REQUEST_SIGN_IN_LOGIN = 1002
     //login by code
     const val REQUEST_SIGN_IN_LOGIN_CODE = 1003
-    /**
-     * your app’s client ID,please replace it of yours
-     */
-    const val CLIENT_ID = "101090009"
+
     /**
      * JWK JSON Web Key endpoint, developer can get the JWK of the last two days from this endpoint
      * See more about JWK in http://self-issued.info/docs/draft-ietf-jose-json-web-key.html
--- Account-Client-Kotlin-Demo/app/src/main/java/com/huawei/logger/LogNode.kt
+++
@@ -1,5 +0,0 @@
-package com.huawei.logger
-
-interface LogNode {
-    fun println(priority: Int, tag: String?, msg: String?, tr: Throwable?)
-}
