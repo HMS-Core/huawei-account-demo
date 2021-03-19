ac61f55e46d59932010d3a56949182d2aa2a0a9f
h00327791 <hexiangbin1@huawei.com>
2021-03-19 16:31:52 +0800

    quickstart demo commit

--- Account-Client-Java-Demo/Account_Demo_Eclipse/HMSSdkBase/gen/com/huawei/android/hms/base/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.huawei.android.hms.base;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Account-Client-Java-Demo/Account_Demo_Eclipse/hmssample/gen/com/huawei/hmssample/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.huawei.hmssample;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
