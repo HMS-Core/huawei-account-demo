b9b881ba680410b1b70777702c76ae894ae17abc
h00327791 <hexiangbin1@huawei.com>
2021-05-31 17:29:48 +0800

    First commit

---
+++ Android_QuickStart_Scenarios_Demo/README_ZH.md
@@ -0,0 +1,44 @@
+# 华为帐号服务示例代码
+
+中文 | [English](https://github.com/HMS-Core/huawei-account-demo/blob/master/README.md) 
+
+## 目录
+* [简介](#简介)
+* [安装](#安装)
+* [环境要求](#环境要求)
+* [配置](#配置)
+* [示例代码](#示例代码)
+* [运行结果](#运行结果)
+* [授权许可](#授权许可)
+
+## 简介
+Android_QuickStart_Scenarios_Demo示例代码对华为帐号服务（HUAWEI Account Kit）的服务端接口进行封装，包含丰富的示例程序，方便您参考或直接使用。详情如下：
+示例代码包，可实现登录、静默登录和退出帐号功能。
+
+## 安装
+如需使用本示例代码中的各种功能，请确保您的设备上已安装华为移动服务（HMS Core）5.0。
+## 环境要求
+推荐使用Android SDK 23及以上版本、JDK 1.8及以上版本。
+
+## 配置
+
+## 示例代码
+快速接入华为帐号场景的客户端Java示例代码提供了以下使用场景：
+1. 登录授权
+2. 静默登录
+3. 获取用户id(openid、unionid)、email、profile（昵称、头像）等
+4. 退出帐号。
+5. 帐号取消授权
+
+## 运行结果
+demo运行最后获取到用户id(openid、unionid)、email、profile（昵称、头像）等。
+
+## 更多详情
+如需了解更多HMS Core相关信息，请前往[Reddit](https://www.reddit.com/r/HuaweiDevelopers/)社区获取HMS Core最新资讯，参与开发者讨论。
+如您对示例代码使用有疑问，请前往：
+* [Stack Overflow](https://stackoverflow.com/questions/tagged/huawei-mobile-services)提问，上传问题时请打上huawei-mobile-services标签。
+* [华为开发者论坛](https://developer.huawei.com/consumer/cn/forum/block/hms-core)，获得更多意见与建议。
+如您在运行示例代码时出现错误，请到GitHub提交[issue](https://github.com/HMS-Core/huawei-account-demo/issues)或[pull request](https://github.com/HMS-Core/huawei-account-demo/pulls)。
+
+## 授权许可
+Android_QuickStart_Scenarios_Demo示例代码经过[Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0)授权许可。
---
+++ Android_QuickStart_Scenarios_Demo/app/src/main/java/com/huawei/quickstartdemo/MainActivity.java
@@ -0,0 +1,188 @@
+package com.huawei.quickstartdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.common.ApiException;
+import com.huawei.hms.support.account.AccountAuthManager;
+import com.huawei.hms.support.account.request.AccountAuthParams;
+import com.huawei.hms.support.account.request.AccountAuthParamsHelper;
+import com.huawei.hms.support.account.result.AuthAccount;
+import com.huawei.hms.support.account.service.AccountAuthService;
+
+public class MainActivity extends AppCompatActivity {
+	// 华为帐号登录授权服务，提供静默登录接口silentSignIn，获取前台登录视图getSignInIntent，登出signOut等接口
+	// Huawei account service, provides silent signIn API silentSignIn, obtain front-end sign-in view API getSignInIntent, sign out API signOut and other APIs
+	private AccountAuthService mAuthService;
+
+	// 华为帐号登录授权参数
+	// parameter
+	private AccountAuthParams mAuthParam;
+
+	// 用户自定义signInIntent请求码
+	// User-defined signInIntent request code
+	private static final int REQUEST_CODE_SIGN_IN = 1000;
+
+	// 用户自定义日志标记
+	// User-defined log mark
+	private static final String TAG = "Account";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// activity_main为自定义布局文件名称
+		// activity_main is the name of the custom layout file
+		setContentView(R.layout.activity_main);
+		findViewById(R.id.HuaweiIdAuthButton).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				silentSignInByHwId();
+			}
+		});
+		findViewById(R.id.HuaweiIdSignOutButton).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				signOut();
+			}
+		});
+
+		findViewById(R.id.HuaweiIdCancelAuthButton).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				cancelAuthorization();
+			}
+		});
+
+	}
+
+	/**
+	 * 静默登录，如果设备上的华为帐号系统帐号已经登录，并且用户已经授权过，无需再拉起登录页面和授权页面，
+	 * 将直接静默登录成功，在成功监听器中，返回帐号信息;
+	 * 如果华为帐号系统帐号未登录或者用户没有授权，静默登录会失败，需要显示拉起前台登录授权视图。
+	 * Silent sign in, if the HUAWEI ID system account on the device has been logged in and
+	 * the user has been authorized, there is no need to pull up the login page and authorization page,
+	 * and the silent login will be successful. In the success monitor, the account information will be returned;
+	 * If the HUAWEI ID system account is not logged in or the user is not authorized, the silent login will fail,
+	 * and the front-end login authorization view needs to be displayed.
+	 */
+	private void silentSignInByHwId() {
+		// 1、配置登录请求参数AccountAuthParams，包括请求用户id(openid、unionid)、email、profile（昵称、头像）等。
+		// 2、DEFAULT_AUTH_REQUEST_PARAM默认包含了id和profile（昵称、头像）的请求。
+		// 3、如需要请求获取用户邮箱，需要setEmail();
+		// 1. Configure the login request parameters AccountAuthParams, including the requested user id (openid, unionid),
+		// email, profile (nickname, avatar), etc.
+		// 2. DEFAULT_AUTH_REQUEST_PARAM includes requests for id and profile (nickname, avatar) by default.
+		// 3. If you need to get the user mailbox again, you need setEmail();
+		mAuthParam = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+				.setEmail()
+				.createParams();
+
+		// 使用请求参数构造华为帐号登录授权服务AccountAuthService
+		// Use request parameters to construct a Huawei account login authorization service AccountAuthService
+		mAuthService = AccountAuthManager.getService(this, mAuthParam);
+
+		// 使用静默登录进行华为帐号登录
+		// Use silent sign in for HUAWEI ID login
+		Task<AuthAccount> task = mAuthService.silentSignIn();
+		task.addOnSuccessListener(new OnSuccessListener<AuthAccount>() {
+			@Override
+			public void onSuccess(AuthAccount authAccount) {
+				// 静默登录成功，处理返回的帐号对象AuthAccount，获取帐号信息
+				// Silent sign in is successful, the returned account object AuthAccount is processed,account information is obtained and processed
+				dealWithResultOfSignIn(authAccount);
+			}
+		});
+		task.addOnFailureListener(new OnFailureListener() {
+			@Override
+			public void onFailure(Exception e) {
+				// 静默登录失败，使用getSignInIntent()方法进行前台显式登录
+				// Silent sign in fails, use the getSignInIntent() method to log in from the foreground
+				if (e instanceof ApiException) {
+					ApiException apiException = (ApiException) e;
+					Intent signInIntent = mAuthService.getSignInIntent();
+					startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN);
+				}
+			}
+		});
+	}
+
+	/**
+	 * 处理返回的AuthAccount，获取帐号信息
+	 * Process the returned AuthAccount and get account information
+	 *
+	 * @param authAccount AccountAccount对象，用于记录帐号信息(AccountAccount object, used to record account information)
+	 */
+	private void dealWithResultOfSignIn(AuthAccount authAccount) {
+		//获取帐号信息
+		Log.i(TAG, "display name:" + authAccount.getDisplayName());
+		Log.i(TAG, "photo uri string:" + authAccount.getAvatarUriString());
+		Log.i(TAG, "photo uri:" + authAccount.getAvatarUri());
+		Log.i(TAG, "email:" + authAccount.getEmail());
+		Log.i(TAG, "openid:" + authAccount.getOpenId());
+		Log.i(TAG, "unionid:" + authAccount.getUnionId());
+		// TODO 获取用户信息后业务逻辑
+		// TODO Business logic after obtaining user information
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == REQUEST_CODE_SIGN_IN) {
+			Log.i(TAG, "onActivitResult of sigInInIntent, request code: " + REQUEST_CODE_SIGN_IN);
+			Task<AuthAccount> authAccountTask = AccountAuthManager.parseAuthResultFromIntent(data);
+			if (authAccountTask.isSuccessful()) {
+				// 登录成功，获取到登录帐号信息对象authAccount
+				// The login is successful, and the login account information object authAccount is obtained
+				AuthAccount authAccount = authAccountTask.getResult();
+				dealWithResultOfSignIn(authAccount);
+				Log.i(TAG, "onActivitResult of sigInInIntent, request code: " + REQUEST_CODE_SIGN_IN);
+			} else {
+				// 登录失败，status code标识了失败的原因，请参考API中的错误码参考了解详细错误原因
+				// Login failed. The status code identifies the reason for the failure. Please refer to the error
+				// code reference in the API for detailed error reasons.
+				Log.e(TAG, "sign in failed : " + ((ApiException) authAccountTask.getException()).getStatusCode());
+			}
+		}
+	}
+
+	private void signOut() {
+		Task<Void> signOutTask = mAuthService.signOut();
+		signOutTask.addOnSuccessListener(new OnSuccessListener<Void>() {
+			@Override
+			public void onSuccess(Void aVoid) {
+				Log.i(TAG, "signOut Success");
+			}
+		}).addOnFailureListener(new OnFailureListener() {
+			@Override
+			public void onFailure(Exception e) {
+				Log.i(TAG, "signOut fail");
+			}
+		});
+	}
+
+	private void cancelAuthorization() {
+		Task<Void> task = mAuthService.cancelAuthorization();
+		task.addOnSuccessListener(new OnSuccessListener<Void>() {
+			@Override
+			public void onSuccess(Void aVoid) {
+				Log.i(TAG, "cancelAuthorization success");
+			}
+		});
+		task.addOnFailureListener(new OnFailureListener() {
+			@Override
+			public void onFailure(Exception e) {
+				Log.i(TAG, "cancelAuthorization failure：" + e.getClass().getSimpleName());
+			}
+		});
+	}
+
+
+}
